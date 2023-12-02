1d60270df4b3448d7a904fa0a3d9442db23db676
jorge.marquez.luna@uabc.edu.mx <jorge.marquez.luna@uabc.edu.mx>
2023-12-01 17:22:28 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MiAplicacion
+{
+    class Program
+    {
+        static void Main()
+        {
+            //correr solo la primera vez para crear la base de datos auxiliar syncAux
+            Db db = new Db();
+            string connectionString = "Data Source=localhost;Initial Catalog=master;User ID=sa;Password=Soluciones@01";
+            db.Conectar(connectionString);
+
+            //correr para sincronizar cada cierto tiempo.
+            SyncDb sync=new SyncDb();
+            string source="Data Source=localhost;Initial Catalog=adCENTRO_FLORICULTOR_D;User ID=sa;Password=Soluciones@01";
+            string target="Data Source=localhost;Initial Catalog=syncAux;User ID=sa;Password=Soluciones@01";
+            sync.SyncTables(source,target);
+        }
+    }
+}
